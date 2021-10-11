cc9fd279c87e14d8aa7029cfbc68781d592d220c
Chai Hyun <91594465+Chai-Hyun@users.noreply.github.com>
2021-10-11 15:40:29 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
 import discord
+import asyncio
+from profanity_filter import ProfanityFilter
 
 my_secret = os.environ['TOKEN']
 
 client = discord.Client()
+
+pf = ProfanityFilter()
+
+@property
+def name(self):
+  return self._name
 
 @client.event
 async def on_ready():
@@ -12,13 +20,26 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.author == client.user:
-
     return
 
-  if message.content.startswith("$hello"):
+  if message.content.startswith("hello"):
     await message.channel.send("Hello!")
 
-  if message.content.startswith("!Gryffindor"):
-    await message.channel.send("Their daring, nerve and chivalry set Gryffindors apart")
+  if message.content.startswith("botname"):
+    await message.channel.send("My name is {0.user}".format(client))
+
+  if message.content.startswith("change name"):
+    await message.channel.send("To which name do you want to change?")
+
+    def check(msg):
+      return msg.author == message.author
+
+    try:
+      new_name=await client.wait_for('message', check=check, timeout=10);
+    except asyncio.TimeoutError:
+      await message.channel.send("Timeout!")
+      return
+
+    await message.guild.me.edit(nick=new_name.content)
 
 client.run(my_secret)
